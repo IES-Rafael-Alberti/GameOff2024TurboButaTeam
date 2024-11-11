c5ff8169a53a1f23bd58940eed99477a9bd62593
EduardoDazaVillanueva <edazvil878@g.educaand.es>
2024-11-11 12:59:22 +0100

    add boss and player life and boss base mechanics

--- scripts/boss.gd
+++ scripts/boss.gd
@@ -1,14 +1,29 @@
 extends Node2D
 
 @onready var timer: Timer = $Timer
+@onready var progress_bar: ProgressBar = $ProgressBar
+
+@export var maxHealthBoss = 200
 
 func _ready() -> void:
-	pass # Replace with function body.
+	GameManager.BossTakeDamage.connect(UpdateProgressBar)
+	GameManager.healthBoss = maxHealthBoss
+	progress_bar.max_value = GameManager.healthBoss
+	progress_bar.value = GameManager.healthBoss
 
 func _process(delta: float) -> void:
 	if !GameManager.isPlayerPhase:
 		#TODO add funcion que realice la accion
+		UseHability()
 		GameManager.isPlayerPhase = true
-		print(GameManager.isPlayerPhase)
 
 #TODO funcion que realice la accion del boss y al finalizar se ponga "GameManager.isPlayerPhase" en true
+func UseHability():
+	print("hace habilidad")
+
+func UpdateProgressBar():
+	progress_bar.value = GameManager.healthBoss
+	
+	if progress_bar.value <= 0:
+		#TODO hacer que el boss se muera
+		print("ganaste")
