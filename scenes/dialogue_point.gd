d76bd882e76a71c4c943d94904aca89bead0666d
Javier Jiménez-Alfaro Hacha <javijjah@gmail.com>
2024-11-15 09:50:06 +0100

    firstcinematicAdjusted

--- scenes/dialogue_point.gd
+++ scenes/dialogue_point.gd
@@ -1,7 +1,7 @@
-extends Node2D
+extends Marker2D
 
 func _ready():
-	await get_tree().create_timer(5)
-	var layout = Dialogic.Styles.load_style("textbubble")
-	layout.register_character(load("res://dialogue/characters/testboss.dch"), $".")
-	Dialogic.start("res://dialogue/timelines/BF1Start.dtl")
+	pass
+	#var layout = Dialogic.Styles.load_style("textbubble")
+	#layout.register_character(load("res://dialogue/characters/testboss.dch"), $".")
+	#Dialogic.start("res://dialogue/timelines/BF1/BF1Start.dtl")
