e48153f62ee558a358261042d1822caaccaa6ba7
Javier Jiménez-Alfaro Hacha <javijjah@gmail.com>
2024-11-18 11:43:34 +0100

    ox dialogue advanced

--- scenes/dialogue_point.gd
+++ scenes/dialogue_point.gd
@@ -3,4 +3,4 @@ extends Marker2D
 func _ready():
 	var layout = Dialogic.Styles.load_style("textbubble")
 	layout.register_character(load("res://dialogue/characters/testboss.dch"), $".")
-	Dialogic.start("res://dialogue/timelines/BF1/BF1Start.dtl")
+	Dialogic.start("res://dialogue/timelines/BF1_OX/OxStart.dtl")
