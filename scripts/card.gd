7dc365f9078f1642de9d83abcf88449f54efead9
Eduardo Daza <edazvil878@g.educaand.es>
2024-11-19 12:23:21 +0100

    Double shift and mechanics of special cards

--- scripts/card.gd
+++ scripts/card.gd
@@ -2,6 +2,8 @@ extends TextureButton
 
 @export var face = Texture
 @export var scriptAnimal: Script
+@export var faceSpecial1 = Texture 
+@export var faceSpecial2 = Texture 
 @onready var animationPlayer = $AnimationPlayer
 
 var back
@@ -10,6 +12,7 @@ var countCouple = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print(face)
 	back = GameManager.cardBack
 	set_texture_normal(back)
 
@@ -32,17 +35,22 @@ func _on_pressed():
 				GameManager.QuitPlayerShield.emit()
 				GameManager.canFlip = false
 				await get_tree().create_timer(1).timeout
+				isSpecialCard(GameManager.firstCardPicked)
 				GameManager.canFlip = true
 		elif GameManager.firstCardPicked && !GameManager.secondCardPicked:
 				GameManager.secondCardPicked = self 
 				GameManager.canFlip = false
 				await get_tree().create_timer(1.5).timeout
+				if !isSpecialCard(GameManager.firstCardPicked, GameManager.secondCardPicked):
+					isEqual(GameManager.firstCardPicked , GameManager.secondCardPicked)
 				GameManager.canFlip = true
-				isEqual(GameManager.firstCardPicked , GameManager.secondCardPicked)
-				GameManager.isPlayerPhase = false
+				
+				if GameManager.doubleShift:
+					GameManager.doubleShift = false
+				else:
+					GameManager.isPlayerPhase = false
 
 func isEqual(firstCard, secondCard):
-
 	if firstCard.get_texture_normal() != secondCard.get_texture_normal():
 		firstCard.voltear()
 		secondCard.voltear()
@@ -57,3 +65,23 @@ func isEqual(firstCard, secondCard):
 		GameManager.countCouple = 0
 	GameManager.firstCardPicked = null
 	GameManager.secondCardPicked = null
+
+func isSpecialCard(firstCard: Object = null, secondCard: Object = null) -> bool:
+	if firstCard != null:
+		if firstCard.get_texture_normal() == faceSpecial2 || firstCard.get_texture_normal() == faceSpecial1:
+			var finalScriptAnimal = scriptAnimal.new()
+			finalScriptAnimal.action()
+			GameManager.firstCardPicked = null
+			GameManager.secondCardPicked = null
+			return true
+	
+	if secondCard != null:
+		if secondCard.get_texture_normal() == faceSpecial2 || secondCard.get_texture_normal() == faceSpecial1:
+			var finalScriptAnimal = scriptAnimal.new()
+			finalScriptAnimal.action()
+			firstCard.voltear()
+			GameManager.firstCardPicked = null
+			GameManager.secondCardPicked = null
+			return true
+	
+	return false
