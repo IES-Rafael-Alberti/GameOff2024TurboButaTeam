6b354a4d911c37911a3212ba7c16445872f8618a
Javier Jiménez-Alfaro Hacha <javijjah@gmail.com>
2024-11-19 21:07:10 +0100

    splash screen main menu and options started

---
+++ scenes/menus/main_menu/main_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var v_box_container: VBoxContainer = $VBoxContainer
+@onready var exit_button: Button = $VBoxContainer/VBoxContainer/ExitButton
+
+func _ready():
+	v_box_container.set_modulate(Color(0,0,0,0))
+	animation_player.play("fade_in")
+	print(OS.get_name())
+	if OS.get_name()=="Web":
+		exit_button.hide()
+
+func _on_play_button_pressed():
+	animation_player.play("fade_out")
+	await animation_player.animation_finished
+	get_tree().change_scene_to_file("res://scenes/cinematics/first_cinematic.tscn")
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
