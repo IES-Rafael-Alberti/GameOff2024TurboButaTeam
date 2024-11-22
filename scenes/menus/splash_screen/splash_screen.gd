09c5e4812dc11b8d4542adf927d97898848c41e9
Javier Jiménez-Alfaro Hacha <javijjah@gmail.com>
2024-11-22 09:58:44 +0100

    salvada main u

--- scenes/menus/splash_screen/splash_screen.gd
+++ scenes/menus/splash_screen/splash_screen.gd
@@ -6,4 +6,4 @@ func _ready() -> void:
 	animation_player.play("splash_screen")
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	get_tree().change_scene_to_file("res://scenes/menus/main_menu/main_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/menus/main_menu/main_menu_fix.tscn")
