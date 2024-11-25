76f77ee4e5302529dda83cc904a478297c8c2667
Javier Jiménez-Alfaro Hacha <javijjah@gmail.com>
2024-11-25 11:30:18 +0100

    figure not appearing fixed

--- scenes/cinematics/second_cinematic.gd
+++ scenes/cinematics/second_cinematic.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	animation_player.play("start")
+	animation_player.play("start2")
 	await animation_player.animation_finished
 	Dialogic.start("res://dialogue/timelines/SecondCinematic/second_cinematic.dtl")
 	await Dialogic.timeline_ended
