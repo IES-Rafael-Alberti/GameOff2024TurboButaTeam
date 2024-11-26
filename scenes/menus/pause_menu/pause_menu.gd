ceb40434eb6c4b529d76d67ced683f8e05e4a3a7
EduardoDazaVillanueva <edazvil878@g.educaand.es>
2024-11-26 22:42:44 +0100

    wiki

--- scenes/menus/pause_menu/pause_menu.gd
+++ scenes/menus/pause_menu/pause_menu.gd
@@ -1,10 +1,7 @@
 extends Control
 
-func _ready():
-	pass
-
 func _on_resume_button_pressed():
-	get_tree().queue_free()
+	self.visible = false
 
 func _on_quit_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/menus/main_menu/main_menu.tscn")
--- scenes/menus/wiki_screen/wiki_screen.gd
+++ scenes/menus/wiki_screen/wiki_screen.gd
@@ -6,10 +6,25 @@ extends Control
 @onready var bg_label: Label = $BGLabel
 @onready var wiki_h_box: Node = $WikiHBox
 @onready var texture: TextureRect = $WikiHBox/CenterContainer/Texture
+@onready var info_panel: Control = $WikiHBox/infoPanel
+@onready var nombre: Label = $WikiHBox/infoPanel/ScrollContainer/VBoxContainer/nombre
+@onready var dato: Label = $WikiHBox/infoPanel/ScrollContainer/VBoxContainer/dato
+@onready var utilidad: Label = $WikiHBox/infoPanel/ScrollContainer/VBoxContainer/utilidad
 
 @export var cardImgList: Array[Texture] = []
 @export var bossImgList: Array[Texture] = []
 @export var bgImgList: Array[Texture] = []
+
+@export var cardNameList: Array[String] = []
+@export var bossNameList: Array[String] = []
+@export var bgNameList: Array[String] = []
+
+@export var cardTextList: Array[String] = []
+@export var bossTextList: Array[String] = []
+@export var bgTextList: Array[String] = []
+
+@export var cardHabilityList: Array[String] = []
+@export var bossHabilityList: Array[String] = []
 
 var current_index_card: int = 0
 var current_index_boss: int = 0
@@ -17,12 +32,14 @@ var current_index_bg: int = 0
 
 func _ready():
 	warning_label.visible = true
+	info_panel.visible = false
 
 func _on_cards_pressed():
 	warning_label.visible = false
 	bosses_label.visible = false
 	bg_label.visible = false
 	cards_label.visible = true
+	info_panel.visible = true
 	
 	if cardImgList.size() > 0:
 		update_image()
@@ -32,6 +49,7 @@ func _on_bosses_pressed():
 	bosses_label.visible = true
 	bg_label.visible = false
 	cards_label.visible = false
+	info_panel.visible = true
 	
 	if bossImgList.size() > 0:
 		update_image()
@@ -41,6 +59,7 @@ func _on_backgrounds_pressed():
 	bosses_label.visible = false
 	bg_label.visible = true
 	cards_label.visible = false
+	info_panel.visible = true
 	
 	if bgImgList.size() > 0:
 		update_image()
@@ -61,7 +80,6 @@ func _on_left_arrow_pressed():
 		if bgImgList.size() > 0:
 			current_index_bg = (current_index_bg - 1 + bgImgList.size()) % bgImgList.size()
 			update_image()
-
 
 # Cambiar a la siguiente imagen
 func _on_right_arrow_pressed():
@@ -84,13 +102,20 @@ func _on_right_arrow_pressed():
 func update_image():
 	if cards_label.visible:
 		texture.texture = cardImgList[current_index_card]
+		nombre.text = cardNameList[current_index_card]
+		dato.text = cardTextList[current_index_card]
+		utilidad.text = cardHabilityList[current_index_card]
 	
 	if bosses_label.visible:
 		texture.texture = bossImgList[current_index_boss]
+		nombre.text = bossNameList[current_index_boss]
+		dato.text = bossTextList[current_index_boss]
+		utilidad.text = bossHabilityList[current_index_boss]
 	
 	if bg_label.visible:
 		texture.texture = bgImgList[current_index_bg]
-
+		nombre.text = bgNameList[current_index_bg]
+		dato.text = bgTextList[current_index_bg]
 
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menus/main_menu/main_menu.tscn")
