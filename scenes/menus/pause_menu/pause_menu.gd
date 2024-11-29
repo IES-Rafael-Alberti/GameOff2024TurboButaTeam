b2714ea7dacd81981e181085021ba43d4c758653
Your Name <sergio2bp@gmail.com>
2024-11-29 09:18:28 +0100

    fix pause menu and add translations

--- scenes/menus/pause_menu/pause_menu.gd
+++ scenes/menus/pause_menu/pause_menu.gd
@@ -1,12 +1,14 @@
 extends Control
 
+@onready var options_menu_pause = $OptionsMenuPause
+
 func _on_resume_button_pressed():
 	self.visible = false
+
 
 func _on_quit_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/menus/main_menu/main_menu.tscn")
 
 
 func _on_resume_button_2_pressed() -> void:
-	var options = preload("res://scenes/options_menu/options_menu_pause.tscn")
-	get_tree().root.add_child(options.instantiate())
+	options_menu_pause.visible = true
